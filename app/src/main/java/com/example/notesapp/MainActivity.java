9f237a0020220eb61b6bd5b2519a648e1c2ffd03
Konrad Wodnicki <56640711+konradw98@users.noreply.github.com>
2020-05-14 11:33:17 +0200

    anroid studio doesn't see second activity

--- app/src/main/java/com/example/notesapp/MainActivity.java
+++ app/src/main/java/com/example/notesapp/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.notesapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> notes = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lisView= findViewById(R.id.listView);
+        notes.add("Example Notes");
+ 
+
+        ArrayAdapter arrayAdapter= new ArrayAdapter(this, android.R.layout.simple_gallery_item, notes);
+        lisView.setAdapter(arrayAdapter);
+
     }
 }
