e2e76306578f104f342708f05e537ef15d7de0fa
Konrad Wodnicki <56640711+konradw98@users.noreply.github.com>
2020-05-14 14:57:07 +0200

    feat
    ability to add a new note

--- app/src/main/java/com/example/notesapp/MainActivity.java
+++ app/src/main/java/com/example/notesapp/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.notesapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -10,19 +18,58 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ArrayList<String> notes = new ArrayList<>();
+   static ArrayList<String> notes = new ArrayList<>();
+   static ArrayAdapter arrayAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView lisView= findViewById(R.id.listView);
+        ListView listView= findViewById(R.id.listView);
         notes.add("Example Notes");
  
 
-        ArrayAdapter arrayAdapter= new ArrayAdapter(this, android.R.layout.simple_gallery_item, notes);
-        lisView.setAdapter(arrayAdapter);
+        arrayAdapter= new ArrayAdapter(this, android.R.layout.simple_gallery_item, notes);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(),NoteEditorActivity.class);
+                intent.putExtra("noteId",position);
+                startActivity(intent);
+            }
+        });
+
+       
 
     }
+
+    // MENU
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if(item.getItemId()==R.id.add_note){
+            Intent intent = new Intent(getApplicationContext(),NoteEditorActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return false;
+
+    }
+
 }
--- app/src/main/java/com/example/notesapp/NoteEditorActivity.java
+++ app/src/main/java/com/example/notesapp/NoteEditorActivity.java
@@ -2,13 +2,46 @@ package com.example.notesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 public class NoteEditorActivity extends AppCompatActivity {
-
+    int noteId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.);
+        setContentView(R.layout.activity_note_editor);
+
+        EditText editText =findViewById(R.id.editText);
+        Intent intent= getIntent();
+         noteId= intent.getIntExtra("noteId",-1);
+
+        if (noteId!=-1) {
+            editText.setText(MainActivity.notes.get(noteId));
+        } else {
+            MainActivity.notes.add("");
+            noteId=MainActivity.notes.size()-1;
+        }
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            MainActivity.notes.set(noteId, String.valueOf(s));
+            MainActivity.arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
 }
